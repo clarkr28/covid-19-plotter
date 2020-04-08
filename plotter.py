c8caa5cd3ec25736e76171dbadd163dfc55aa58e
Jared Clark <clarkr28@msu.edu>
2020-04-08 14:32:54 -0400

    now works with counties

--- plotter.py
+++ plotter.py
@@ -12,33 +12,44 @@ register_matplotlib_converters()
 STATES_MAPPING_FNAME = 'state-codes.json'
 STATES_FNAME = 'data/us-states.csv'
 # columns: date, state, fips, cases, deaths
-COUNTIES_FNAME = 'covid-19-data/us-counties.csv'
+COUNTIES_FNAME = 'data/us-counties.csv'
 # columns: date, county, state, fips, cases, deaths
 
 
 def create_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-s', '--states', nargs='+', help='States to display', 
-        default=list(), dest='states')
+    parser.add_argument('-k', '--keys', nargs='+', help='list State or State:County', 
+        default=list(), dest='keys')
     parser.add_argument('-d', '--deaths', help='plot cumulative deaths', 
         dest='plot_deaths', action='store_true')
     return parser
 
 
 
-def process_state_input(state_args):
+def process_state_input(keys):
     """replace state abbreviations with their full name"""
 
-    if len(state_args) == 0:
+    if len(keys) == 0:
         # there's no need to spend time loading and processing the mapping file
-        return state_args
+        return keys
         
     # load the state abbreviations
     f = open(STATES_MAPPING_FNAME, 'r')
     mapping = json.load(f)
     f.close()
+
     # replace state abbreviations with their full name if abbreviation found
-    return [mapping[entry] if entry in mapping else entry for entry in state_args]
+    output = []
+    for entry in keys:
+        if ':' in entry:
+            state, county = entry.split(':')[0:2]
+            if state in mapping:
+                output.append('{}:{}'.format(mapping[state], county))
+            else:
+                output.append(entry)
+        else:
+            output.append(mapping[entry] if entry in mapping else entry)
+    return output
 
 
     
@@ -47,24 +58,32 @@ if __name__ == '__main__':
     parser = create_parser()
     args = parser.parse_args()
 
-    df = pd.read_csv(STATES_FNAME)
-    selected_states = process_state_input(args.states)
-    if len(selected_states) == 0:
-        selected_states = sorted(list(df.state.unique()))
+    s_df = pd.read_csv(STATES_FNAME)
+    c_df = pd.read_csv(COUNTIES_FNAME)
+    keys = process_state_input(args.keys)
+    print(keys)
+    if len(keys) == 0:
+        keys = sorted(list(s_df.state.unique()))
     fig, ax = plt.subplots()
     lines = [] # saves lines for use in legend
     mode = 'deaths' if args.plot_deaths else 'cases'
 
     # plot each state one at a time
-    for state in selected_states:
-        state_data = df.loc[df['state'] == state].sort_values(by=['date'])
-        x = [date(int(d[:4]), int(d[5:7]), int(d[8:])) for d in state_data['date'].tolist()]
-        y = state_data[mode].tolist()
+    for key in keys:
+        key_data = s_df[s_df['state'] == 'fake'].sort_values(by=['date']) # empty placeholder
+        if ':' in key:
+            s, c = key.split(':')[0:2]
+            print('{}, {}, {}'.format(key, s, c))
+            key_data = c_df[(c_df['state'] == s) & (c_df['county'] == c)].sort_values(by=['date'])
+        else:
+            key_data = s_df[s_df['state'] == key].sort_values(by=['date'])
+        x = [date(int(d[:4]), int(d[5:7]), int(d[8:])) for d in key_data['date'].tolist()]
+        y = key_data[mode].tolist()
         line, = ax.plot(x, y, marker='.')
         lines.append(line)
 
     # formatting plot
-    ax.legend(lines, selected_states)
+    ax.legend(lines, keys)
     ax.xaxis.set_major_locator(MultipleLocator(7))
     ax.xaxis.set_minor_locator(MultipleLocator(1))
     ax.tick_params(which='major', length=8, width=2)
